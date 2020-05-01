383ad542cc2b6164351f76f3335bd1f84578a0b9
Brody Nagy <nagyb@wit.edu>
2020-05-01 16:56:19 -0400

    basically 95% totally done :)

--- src/components/NoteEntry.jsx
+++ src/components/NoteEntry.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
 
 export class NoteEntry extends Component {
 	static propTypes = {
@@ -10,15 +11,53 @@ export class NoteEntry extends Component {
 		super(props);
 
 		this.state= {
-
+			title: null,
+			category: null,
+			note: null
 		};
 	}
 
 	render(){
 
-		return (
-			<h2> Notely </h2>
-		)
+		console.log(this.state);
+		return (	
+			<div>
+				<h2 id={"title"}> Notely </h2>
+				<Form>
+					<FormGroup>
+						<InputGroup id="note_title_entry">
+							<InputGroupAddon addonType="prepend">
+								<InputGroupText>Title</InputGroupText>
+							</InputGroupAddon>
+							<Input
+								type="text"
+								placeholder="Enter your Title here!"
+								onBlur={(event) => { this.setState({title:event.target.value}); }}
+							/>
+						</InputGroup>
+					</FormGroup>
+
+					<FormGroup>
+						<InputGroup id="note_category_entry">
+							<InputGroupAddon addonType="prepend">
+								<InputGroupText>Category</InputGroupText>
+							</InputGroupAddon>
+							<Input 
+								type="text"
+								placeholder="Enter your Category here!" 
+								onBlur={(event) => { this.setState({category:event.target.value}); }}
+							/>
+						</InputGroup>
+					</FormGroup>
+
+					<FormGroup>
+						<Input type="textarea" name="note" id="note_text_entry" 
+							onBlur={(event) => { this.setState({note:event.target.value}); }}
+						/>
+					</FormGroup>
+				</Form>
+			</div>
+		);
 
 		
 	}
